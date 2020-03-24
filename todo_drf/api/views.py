a49907b576e44bd7eee891b26155db5d80d506c5
Ahmed <Sharif.Ahmed@dxc.com>
2020-03-24 17:35:23 +0000

    first commit

---
+++ todo_drf/api/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
+
+# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+@api_view(['GET'])
+def taskList(request):
+    tasks = task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
