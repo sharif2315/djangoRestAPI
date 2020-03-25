a831b05d0fb297b54d68c16a278d7f3057ddec2e
Ahmed <sharif.ahmed@dxc.com>
2020-03-25 12:25:45 +0000

    completed api crud

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -18,8 +18,45 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+
 @api_view(['GET'])
 def taskList(request):
-    tasks = task.objects.all()
+    tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Ietm successfully deleted!")
